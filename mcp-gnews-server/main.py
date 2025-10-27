be18c5fc14dd3c344bbddf0c1d99ee5b930c9957
Brent Deaver <brent_deaver@Brents-MacBook-Pro.local>
2025-10-26 17:09:33 -0700

    #2: Configure logger

--- mcp-gnews-server/main.py
+++ mcp-gnews-server/main.py
@@ -1,5 +1,22 @@
+import os
+import logging
+import mcp.server.fastmcp as FastMCP
+import httpx
+
+# Configure logger
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+# Init logger
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello from mcp-gnews-server!")
+    logger.info("Starting MCP GNews Serve")
+    mcp.run("stdio")
 
 
 if __name__ == "__main__":
